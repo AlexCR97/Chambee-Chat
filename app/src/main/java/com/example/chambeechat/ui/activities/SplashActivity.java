b50bca9795ab59e9bd215a88369f17777e7befb9
AlexCR97 <carp_97@outlook.com>
2020-02-12 18:07:34 -0600

    Commit inicial

---
+++ app/src/main/java/com/example/chambeechat/ui/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.chambeechat.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.chambeechat.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
