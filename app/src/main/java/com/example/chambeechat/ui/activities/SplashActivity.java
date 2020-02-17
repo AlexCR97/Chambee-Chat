9e1981b741580b8ced288eaf452483e52697bf3d
AlexCR97 <carp_97@outlook.com>
2020-02-17 16:56:16 -0600

    General cleanup

--- app/src/main/java/com/example/chambeechat/ui/activities/SplashActivity.java
+++ app/src/main/java/com/example/chambeechat/ui/activities/SplashActivity.java
@@ -24,7 +24,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         ivLogo = findViewById(R.id.ivLogo);
-        llNombre=findViewById(R.id.llNombre);
+        llNombre = findViewById(R.id.llNombre);
 
         new Handler().postDelayed(new Runnable() {
             @Override
